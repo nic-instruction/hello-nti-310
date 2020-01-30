1075e928247fab60061c3eba29f740499092d00c
nic-instruction <nicole.bade@seattlecolleges.edu>
2020-01-30 14:59:27 -0800

    Update ldap-client.sh

--- ldap-client.sh
+++ ldap-client.sh
@@ -3,3 +3,4 @@ apt -y install libnss-ldap libpam-ldap ldap-utils
 sed -i 's/passwd:         compat systemd/passwd:         compat systemd ldap/g' /etc/nsswitch.conf
 sed -i 's/group:          compat systemd/group:          compat systemd ldap/g' /etc/nsswitch.conf
 sed -i 's/password        \[success=1 user_unknown=ignore default=die\]     pam_ldap.so use_authtok try_first_pass/password        \[success=1 user_unknown=ignore default=die\]     pam_ldap.so try_first_pass/g' /etc/pam.d/common-password
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
