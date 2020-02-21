5ab998481d6a40a308370fd55c1b93e7af1e3029
nic-instruction <nicole.bade@seattlecolleges.edu>
2020-02-20 17:12:33 -0800

    Create django.sh

---
+++ django.sh
@@ -0,0 +1,17 @@
+  
+#!/bin/bash
+# fun django tutorial https://data-flair.training/blogs/django-migrations-and-database-connectivity/
+# These are the really tricky sed and perl lines from the handout.  Please see me for the handout.
+
+# from the django server:
+perl -i -0pe "BEGIN{undef $/;} s/        'ENGINE':.*db.sqlite3'\),/        'ENGINE': 'django.db.backends.postgresql_psycopg2',\n        'NAME': 'nti310',\n        'USER': 'nti310user',\n        'PASSWORD': 'password',\n        'HOST': 'postgres',\n        'PORT': '5432',/smg" /opt/nti310/nti310/settings.py
+
+#From the Postgres sever:
+sed -i "s/host    all             all             127.0.0.1\/32            md5/host    all             all             0.0.0.0\/0               md5/g" /var/lib/pgsql/data/pg_hba.conf
+
+#From the django server:
+# put sed into the INSTALLED_APPS variable
+sed -i "40i \ \ \ \ 'Cars'," nti310/settings.py
+
+#django admin user creation
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@newproject.com','NTI300NTI300')" | python manage.py shell   
