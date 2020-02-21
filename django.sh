d7114bf1d85a41da95929f36617a3e717b957c43
nic-instruction <nicole.bade@seattlecolleges.edu>
2020-02-20 19:07:10 -0800

    Update django.sh

--- django.sh
+++ django.sh
@@ -10,13 +10,13 @@ perl -i -0pe "BEGIN{undef $/;} s/        'ENGINE':.*db.sqlite3'\),/        'ENGI
 sed -i "s/host    all             all             127.0.0.1\/32            md5/host    all             all             0.0.0.0\/0               md5/g" /var/lib/pgsql/data/pg_hba.conf
 
 #From the django server:
-# put sed into the INSTALLED_APPS variable
-sed -i "40i \ \ \ \ 'Cars'," nti310/settings.py
 
 echo "class Specs(models.Model):
     name = models.CharField(max_length = 20)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     weight = models.PositiveIntegerField()" >> Cars/models.py
+
+sed -i "40i \ \ \ \ 'Cars'," nti310/settings.py
 
 #django admin user creation
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@newproject.com','NTI300NTI300')" | python manage.py shell   
