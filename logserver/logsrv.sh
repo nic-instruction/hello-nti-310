20654a106d596307814a921c544632aaaf15ccb9
nic-instruction <nicole.bade@seattlecolleges.edu>
2020-03-05 15:58:00 -0800

    Create logsrv.sh

---
+++ logserver/logsrv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sed -i 's/#$UDPServerRun 514/$UDPServerRun 514/g' /etc/rsyslog.conf
+sed -i 's/#$InputTCPServerRun 514/$InputTCPServerRun 514/g' /etc/rsyslog.conf
+systemctl restart rsyslog
+netstat -antup | grep 514
