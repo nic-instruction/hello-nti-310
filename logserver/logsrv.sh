948d3612c41241e950d4d6c36c26e84852d2e3c6
nic-instruction <nicole.bade@seattlecolleges.edu>
2020-03-05 16:23:16 -0800

    Update logsrv.sh

--- logserver/logsrv.sh
+++ logserver/logsrv.sh
@@ -3,5 +3,6 @@ sed -i 's/#$UDPServerRun 514/$UDPServerRun 514/g' /etc/rsyslog.conf
 sed -i 's/#$InputTCPServerRun 514/$InputTCPServerRun 514/g' /etc/rsyslog.conf
 sed -i 's/#$ModLoad imtcp/$ModLoad imtcp/g' /etc/rsyslog.conf
 sed -i 's/#$ModLoad imudp/$ModLoad imudp/g' /etc/rsyslog.conf
+setenforce 0
 systemctl restart rsyslog
 netstat -antup | grep 514
